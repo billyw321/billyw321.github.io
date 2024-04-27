a86b6151de90a865a1cf24d7153df9b7882f81d4
billy <113080633+billyw321@users.noreply.github.com>
2024-04-27 17:01:58 -0400

    updated structure

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function copyText() {
+
+    navigator.clipboard.writeText("wholesomebilly");
+    console.log(`Copied!`);
+}
+function changeIMG(){
+    document.getElementById("patchy").src = "img/patchy2.gif";
+}
+function changeBCK(){
+    document.getElementById("patchy2").src = "img/patchy.gif";
+}
+
+console.log(`Hello!`);
